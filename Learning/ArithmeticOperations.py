e595c37b7d599f2cab8572a91c24a8e78f0776e1
Kavish Rizvi <rizvikavish205.com>
2022-02-27 11:12:32 +0530

    Arithmetic Code

---
+++ Learning/ArithmeticOperations.py
@@ -0,0 +1,13 @@
+number = 1 + 2 * 3 / 4.0
+print(number)
+remainder = 11 % 3
+print(remainder)
+# Using two multiplication symbols makes a power relationship.
+squared = 7 ** 2
+cubed = 7 ** 3
+print(squared)
+print(cubed)
+helloworld = "hello" + " " + "world"
+print(helloworld)
+lotsofhellos = "hello " * 10
+print(lotsofhellos)
