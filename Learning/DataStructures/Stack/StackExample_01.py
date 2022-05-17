d51912df90cb1816c44924c18fdcd038b34c45ac
Kavish Rizvi <rizvikavish205.com>
2022-05-17 12:29:54 +0530

    Stack

---
+++ Learning/DataStructures/Stack/StackExample_01.py
@@ -0,0 +1,14 @@
+stack =[]
+stack.append('a')
+stack.append('b')
+stack.append('c')
+print('initial Stack')
+print(stack)
+
+print('\nElements popped:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print('\nStack Elements after pop:')
+print(stack)
